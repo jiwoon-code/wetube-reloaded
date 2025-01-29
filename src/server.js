663c8b463bc8db512a22df0206618c1fc849d4f9
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-01-29 20:33:57 +0900

    333

--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
 import express from "express";
 
-const app = express();
-const handleListening = () => console.log("server listening on port 4000")
+const PORT = 4000;
 
-app.listen(4000, handleListening)
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("ulala");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("login here");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+const handleListening = () =>
+  console.log(`server listening on port 4000 http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
