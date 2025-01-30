85269de8ee1fe7cdd7914aa300619ce5e61b28fb
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-01-31 00:19:36 +0900

    middleware

--- src/server.js
+++ src/server.js
@@ -4,16 +4,32 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMidleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>not allowed</h1>");
+  }
+  console.log("allowed");
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("ulala");
+  return res.send("Home");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("login here");
+const handleProtected = (req, res) => {
+  return res.send("it's Private");
 };
 
+app.use(logger);
+app.use(privateMidleware);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`server listening on port 4000 http://localhost:${PORT}`);
