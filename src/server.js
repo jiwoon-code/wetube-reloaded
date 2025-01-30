13b9adbdbd70af8c8a16d82b00c354578c39c832
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-01-31 01:44:33 +0900

    routers

--- src/server.js
+++ src/server.js
@@ -1,22 +1,14 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 app.use(logger);
-
-const globalRouter = express.Router();
-const userRouter = express.Router();
-const videoRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-globalRouter.get("/", handleHome);
-const handleEditUser = (req, res) => res.send("Edit User");
-userRouter.get("/edit", handleEditUser);
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
