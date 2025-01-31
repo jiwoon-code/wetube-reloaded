34edaf427a18d30f83abbbb8539c4190f6dfc6a1
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-01-31 23:33:33 +0900

    params 1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,12 @@
-export const trending = (req, res) => res.send("HOme page Videos");
-export const see = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => res.send("Home page Videos");
+export const see = (req, res) => {
+  res.send(`Watch Video #${req.params.id}`);
+};
+export const edit = (req, res) => {
+  res.send("Edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const deleteVideo = (req, res) => {
+  res.send("Delete Video");
+};
