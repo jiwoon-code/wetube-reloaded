b5c0ab60b3b9f34f632397fe3ecd0337e94b3bda
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-02-03 15:17:47 +0900

    mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,30 @@
 export const trending = (req, res) => {
-  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const videos = [
+    {
+      title: "video 1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 55,
+      id: 1,
+    },
+    {
+      title: "video 2",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 55,
+      id: 1,
+    },
+    {
+      title: "video 3",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 55,
+      id: 1,
+    },
+  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const see = (req, res) => res.render("watch");
