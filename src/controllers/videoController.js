2065ff95c86d9e6af5563b8989bab9d688eb7ccb
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-02-07 02:51:39 +0900

    serach part 2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -64,8 +64,15 @@ export const deleteVideo = async (req, res) => {
   return res.redirect("/");
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const { keyword } = req.query;
-  console.log("should search", keyword);
-  return res.render("search", { pageTitle: "Search" });
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}$`, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
