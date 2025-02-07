7c51a70bfa2ea697dd62c421c6c6827340130e37
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-02-08 01:58:32 +0900

    confirmation error

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,22 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  const exits = await User.exists({ $or: [{ username }, { email }] });
+  if (exits) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/email already exits.",
+    });
+  }
+
   await User.create({
     name,
     username,
