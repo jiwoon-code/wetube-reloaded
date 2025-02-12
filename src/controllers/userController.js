9b10a2c08c4e46ef431eea52b2fefae1b40e4c69
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-02-13 01:14:26 +0900

    github login part 5

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -110,12 +110,30 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-
-    const email = emailData.find(
+    console.log(emailData);
+    const emailObj = emailData.find(
       (email) => email.primary === true && email.verified === true
     );
-    if (!email) {
+    if (!emailObj) {
       res.redirect("/login");
+    }
+    const existingUser = await User.findOne({ email: emailObj.email });
+    if (existingUser) {
+      req.session.loggedIn = true;
+      req.session.user = existingUser;
+      return res.redirect("/");
+    } else {
+      const user = await User.create({
+        name: userData.name,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+      req.session.loggedIn = true;
+      req.session.user = user;
+      return res.redirect("/");
     }
   } else {
     res.redirect("/login");
