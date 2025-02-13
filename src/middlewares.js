94e88af3b7096950d00e806b3984f813b206844a
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-02-13 22:55:15 +0900

    edit profile post 1

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,13 +2,13 @@ export const localsMiddleWare = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
-  console.log(res.locals);
+  console.log(req.session.user);
   next();
 };
 
 export const protectorMiddleware = (req, res, next) => {
   if (req.session.loggedIn) {
-    next();
+    return next();
   } else {
     return res.redirect("/login");
   }
