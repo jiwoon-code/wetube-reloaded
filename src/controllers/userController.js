3a0542d0012b2d865a763fa7ab9859ad8921bfc8
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-02-13 21:03:24 +0900

    edit profile get

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -136,10 +136,16 @@ export const finishGithubLogin = async (req, res) => {
     res.redirect("/login");
   }
 };
-export const edit = (req, res) => res.send("Edit User");
 
 export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
+};
+
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = (req, res) => {
+  return res.render("edit-profile");
 };
 export const see = (req, res) => res.send("See User");
