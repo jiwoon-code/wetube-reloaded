34d7eb55562ea3f3c3847fb716a691db3637c687
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-02-25 00:39:47 +0900

    flash messages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
   entry: {
     main: "./src/client/js/main.js",
     videoPlayer: "./src/client/js/videoPlayer.js",
-    recorder: "./src/client/js/recorder.js",
   },
   mode: "development",
   watch: true,
