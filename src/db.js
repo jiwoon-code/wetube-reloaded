15b67eb4c8afd80b54fe44f00fcf4aac71282cb8
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-02-28 01:21:00 +0900

    delete fetch

--- src/db.js
+++ src/db.js
@@ -5,5 +5,7 @@ mongoose.connect(process.env.DB_URL);
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("Connected to DB");
-db.on("error", (error) => console.log("DB Error", error));
+const handleError = (error) => console.log("DB Error", error);
+
+db.on("error", handleError);
 db.once("open", handleOpen);
